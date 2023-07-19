cb1eb7fb4eaf869a177b4346ebf8a99ffc64de2a
Alexei Popov <alexeipopov.93@gmail.com>
2023-07-19 16:13:08 +0300

    added Dutch translation strings

--- translations.js
+++ translations.js
@@ -84,13 +84,90 @@ const translations = {
   //Bulgarian Language
   BG: {
     heading: 'Запознайте се с най-новия ни продукт EasyServed',
-    aboutUs: 'За Нас',
+    aboutUsNav: 'За Нас',
   },
 
   //Dutch Language
   NL: {
-    heading: 'Запознайте се с най-новия ни продукт EasyServed',
-    aboutUs: 'За Нас',
+    //Nav Bar
+    aboutUsNav: 'Over ons',
+    productsNav: 'Producten',
+    clientsNav: 'Klanten',
+    contactNav: 'Contact',
+    //Title
+    heading: 'Maak kennis met ons nieuwste product EasyServed',
+    learnMore: 'Kom meer te weten',
+    //Services
+    firstServiceTitle: 'IT-oplossingen en advies',
+    firstServiceContent:
+      'Onze IT-oplossingen en adviesdiensten stellen bedrijven in staat met uitgebreide begeleiding, op maat gemaakte strategieën en geavanceerde technologieën om de IT-infrastructuur te optimaliseren, groei te stimuleren en nieuwe hoogten in het digitale landschap te bereiken.',
+    secondServiceTitle: 'Web- en softwareontwikkeling',
+    secondServiceContent:
+      'We creëren boeiende, responsieve websites die uw ideeën omzetten in boeiende online ervaringen, gebruikmakend van de nieuwste technologieën voor naadloze gebruikersinteractie.',
+    thirdServiceTitle: 'Gegarandeerd werken',
+    thirdServiceContent:
+      'Het team bouwt duurzame relaties op en levert innovatieve softwareoplossingen op maat van uw zakelijke behoeften, waarbij technische vaardigheid wordt gecombineerd met grenzeloze creativiteit.',
+    //About us
+    slogan: 'Inspirerende IT-oplossingen. Grenzeloze creativiteit.',
+    firstAboutUsContent:
+      'Bij Remicel zijn we een toonaangevende leverancier van IT-oplossingen, gespecialiseerd in IT-consulting, webontwikkeling en softwareontwikkeling. Met een passie voor het leveren van uitzonderlijke resultaten, stellen we bedrijven in staat om te gedijen in het digitale landschap.',
+    secondAboutUsContent:
+      'Met een klantgerichte benadering geven we prioriteit aan het opbouwen van duurzame relaties en het begrijpen van de unieke behoeften van elke klant. Ons team van bekwame professionals combineert technische expertise met grenzeloze creativiteit om innovatieve en schaalbare oplossingen te creëren die zijn afgestemd op uw specifieke vereisten.',
+    thirdAboutUsContent:
+      'Wij geloven in de kracht van samenwerking en werken nauw samen met onze klanten om hun uitdagingen te analyseren, strategieën te ontwerpen en geavanceerde technologieën te implementeren. Ons doel is om uw IT-infrastructuur te optimaliseren, groei te stimuleren en uw concurrentievoordeel te vergroten.',
+    fourthAboutUsContent:
+      'Met onze uitgebreide IT-adviesdiensten bieden we strategische begeleiding, gebruikmakend van onze branche-inzichten om u te helpen weloverwogen beslissingen te nemen en te navigeren in het steeds evoluerende technologische landschap. Ons webontwikkelingsteam maakt boeiende en responsieve websites die uw ideeën omzetten in boeiende online ervaringen, terwijl onze softwareontwikkelingsexperts robuuste oplossingen leveren om uw activiteiten te stroomlijnen en uw bedrijf vooruit te helpen.',
+    fithAboutUsContent:
+      'Bij Remicel streven we naar uitmuntendheid, betrouwbaarheid en innovatie. Werk met ons samen om het volledige potentieel van uw bedrijf te benutten door middel van inspirerende IT-oplossingen en grenzeloze creativiteit.',
+    //Testimonials
+    firstTestimonial:
+      'De EasyServed-oplossing van Remicel heeft ons bedrijf getransformeerd. Met de QR-codefunctie kunnen gasten eenvoudig bestellen, wat tijd bespaart en personeel vermindert. Integratie met ons kassasysteem verhoogt de operationele efficiëntie.',
+    secondTestimonial:
+      'Het overzichtelijke zitplan van MyTimo stimuleert een maximale bezetting en levert uiteindelijk meer omzet op. Werkt verbazingwekkend snel, dankzij het automatische systeem van Timo.',
+    thirdTestimonial:
+      'Tijdens de coronacrisis moesten we ons creatief aanpassen. We hebben snel een pop-up terras gerealiseerd, dankzij de efficiënte besteloplossing van Remicel. EasyServed is snel, gebruiksvriendelijk en een echte aanrader.',
+    //Products
+    //EasyServed
+    easyServedDescription:
+      'Met EasyServed start je eenvoudig en snel met je eigen bestelsite. Uw gasten plaatsen hun bestelling via hun smartphone, tablet of computer, die ze vervolgens afhalen of laten bezorgen. Vervolgens kunt u uw inkomende bestellingen online volgen, via een bonprinter of gekoppeld aan uw kassasysteem. Naast het verkopen van producten is het ook mogelijk om reserveringen te ontvangen.',
+    firstEasyServedTitle: 'Makkelijk online bestellen en verkopen',
+    firstEasyServedContent:
+      'Dankzij de Qr-code bestellen uw klanten heel eenvoudig en overzichtelijk vanuit huis of op locatie.',
+    secondEasyServedTitle: 'Gepersonaliseerde website',
+    secondEasyServedContent:
+      'Een professionele en volledig gepersonaliseerde website zonder verdere zorgen en alle techniek, … dat is wat EasyServed biedt!',
+    thirdEasyServedTitle: 'Duidelijke rapportage & inzicht in piekmomenten',
+    thirdEasyServedContent:
+      'Op elk moment met één klik een overzicht van uw bestellingen, omzet zowel per dag, maand als per persoon.',
+    fourthEasyServedTitle: 'Snel aan de slag',
+    fourthEasyServedContent:
+      'Eenvoudige backoffice om uw gepersonaliseerde website onmiddellijk operationeel te krijgen.',
+    //MyTimo
+    myTimoDescription:
+      'Timo is een veilig alles-in-één systeem dat kaartverkoop combineert met automatische facturatie en boekhouding, girale betalingen, voorraad- en databeheer. Het systeem is niet alleen veilig, maar ook uiterst gebruiksvriendelijk, nauwkeurig en betaalbaar.',
+    firstMyTimoTitle: 'Ticketsysteem',
+    firstMyTimoContent:
+      'Een duidelijk zitplan is de sleutel tot maximale bezetting. Bovendien kunt u uw bezoekers indelen in specifieke categorieën en verschillende privileges toekennen.',
+    secondMyTimoTitle: 'Cashloze betalingen',
+    secondMyTimoContent:
+      'Dankzij het Timo cashless systeem hebben uw bezoekers geen contant geld meer nodig. Dit is niet alleen veiliger, maar ook efficiënter voor uw organisatie.',
+    thirdMyTimoTitle: 'Automatisch voorraadbeheer',
+    thirdMyTimoContent:
+      'Timo kan uw verkopen automatisch registreren als u gebruik maakt van het cashless systeem. Het is zelfs mogelijk om automatisch bestellingen bij uw leveranciers te plaatsen zodat uw voorraden op tijd aangevuld zijn.',
+    fourthMyTimoTitle: 'Gegevensbeheer',
+    fourthMyTimoContent:
+      'U krijgt een onschatbare bron van informatie die u actief kunt gebruiken ter ondersteuning van uw dagelijkse bedrijfsvoering.',
+    //Call to Action
+    callToAction:
+      'Begin met het implementeren van onze producten in uw bedrijf!',
+    //Contact Us Form
+    contactUsFormTitle: 'Neem contact met ons op',
+    contactUsFormContent:
+      'Vul het onderstaande formulier in om ons een bericht te sturen en we nemen zo snel mogelijk contact met u op.',
+    contactUsFormFullname: 'Voor-en achternaam',
+    contactUsFormEmail: 'Email',
+    contactUsFormMessage: 'Bericht',
+    contactUsFormSubmit: 'Indienen',
   },
   // add more languages here as needed
 };
