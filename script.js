fbc58df519547e9523438937eb7aefabffd900e9
Alexei Popov <alexeipopov.93@gmail.com>
2023-07-21 10:20:31 +0300

    added changes from Simeon

--- script.js
+++ script.js
@@ -147,3 +147,20 @@ $('#myForm').validate({
     form.submit();
   },
 });
+
+//lazy loading script here
+const blurDivs = document.querySelectorAll('.blur-load');
+blurDivs.forEach((div) => {
+  const img = div.querySelector('img');
+
+  function loaded() {
+    div.classList.add('loaded');
+    div.classList.add('hidden');
+  }
+
+  if (img.complete) {
+    loaded();
+  } else {
+    img.addEventListener('load', loaded);
+  }
+});
